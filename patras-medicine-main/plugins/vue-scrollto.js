1ac00deaa913abe5d3ee2a95d67979ce4e7c36a2
Waris Shaikh <shaikhwaris100@gmail.com>
2024-05-25 18:08:51 +0800

    Delete patras-medicine-main directory

--- patras-medicine-main/plugins/vue-scrollto.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import VueScrollTo from "vue-scrollto";
-
-Vue.use(VueScrollTo, {
-  duration: 500,
-  easing: [0.25, 0.1, 0.25, 1.0],
-  offset: -80,
-  cancelable: false,
-});
